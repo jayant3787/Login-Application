d0a9711b7e1c2a3262087529d6255cb6260e2288
Jayant sahu <jayant3787@gmail.com>
2021-06-24 12:37:22 +0530

    fixed casing

--- src/Router/index.js
+++ src/Router/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "../pages/login";
 import Home from "../pages/home";
-import Feedback from "../pages/feedback";
-import Profile from "../pages/profile";
+import Feedback from "../pages/Feedback";
+import Profile from "../pages/Profile";
 
 export default function index() {
   return (
